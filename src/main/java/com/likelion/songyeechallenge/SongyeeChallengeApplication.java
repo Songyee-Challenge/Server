4d99ed3dc4d4692bfa39de060f7c8bde849c51d7
genieu99 <yujin0419@sookmyung.ac.kr>
2023-11-18 00:35:27 +0900

    [Feat] 프로젝트 초기 설정 (#1)

---
+++ src/main/java/com/likelion/songyeechallenge/SongyeeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.songyeechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongyeeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongyeeChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/songyeechallenge/SongyeeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.songyeechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongyeeChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
