a9feaaa5fda892ce66a036d7b43dcc1ca8ccd832
genieu99 <yujin0419@sookmyung.ac.kr>
2023-11-19 21:53:11 +0900

    [Feat] 진행 상황 별 챌린지 조회 (#9)

--- src/main/java/com/likelion/songyeechallenge/controller/ChallengeController.java
+++ src/main/java/com/likelion/songyeechallenge/controller/ChallengeController.java
@@ -2,10 +2,13 @@ package com.likelion.songyeechallenge.controller;
 
 import com.likelion.songyeechallenge.domain.challenge.Challenge;
 import com.likelion.songyeechallenge.service.ChallengeService;
+import com.likelion.songyeechallenge.web.dto.ChallengeListResponseDto;
 import com.likelion.songyeechallenge.web.dto.ChallengeSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/challenge")
@@ -18,5 +21,20 @@ public class ChallengeController {
     public Long postChallenge(@RequestPart(value = "dto") ChallengeSaveRequestDto saveRequestDto, @RequestParam("picture") MultipartFile file) {
         Challenge challenge = challengeService.postChallenge(saveRequestDto, file);
         return challenge.getChallenge_id();
+    }
+
+    @GetMapping("/recruit")
+    public List<ChallengeListResponseDto> getRecruitChallenge() {
+        return challengeService.findRecruitingPost();
+    }
+
+    @GetMapping("/inprocess")
+    public List<ChallengeListResponseDto> getInProcessChallenge() {
+        return challengeService.findInProcessPost();
+    }
+
+    @GetMapping("/finish")
+    public List<ChallengeListResponseDto> getFinishChallenge() {
+        return challengeService.findFinishedPost();
     }
 }
--- src/main/java/com/likelion/songyeechallenge/domain/challenge/ChallengeRepository.java
+++ src/main/java/com/likelion/songyeechallenge/domain/challenge/ChallengeRepository.java
@@ -1,6 +1,17 @@
 package com.likelion.songyeechallenge.domain.challenge;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+    @Query("SELECT c FROM Challenge c WHERE c.startDate > :today ORDER BY c.challenge_id DESC")
+    List<Challenge> findBeforeStartDateDesc(String today);
+
+    @Query("SELECT c FROM Challenge c WHERE c.startDate <= :today AND c.endDate >= :today ORDER BY c.challenge_id DESC")
+    List<Challenge> findBetweenStartDateAndEndDateDesc(String today);
+
+    @Query("SELECT c FROM Challenge c WHERE c.endDate < :today ORDER BY c.challenge_id DESC")
+    List<Challenge> findAfterEndDateDesc(String today);
 }
