318c5286efc1bf96ad7342ef4922e257c67a9187
genieu99 <yujin0419@sookmyung.ac.kr>
2023-11-20 03:02:03 +0900

    [Feat] 메인페이지 구현 (#13)

--- src/main/java/com/likelion/songyeechallenge/domain/challenge/ChallengeRepository.java
+++ src/main/java/com/likelion/songyeechallenge/domain/challenge/ChallengeRepository.java
@@ -14,4 +14,9 @@ public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
     @Query("SELECT c FROM Challenge c WHERE c.endDate < :today ORDER BY c.challenge_id DESC")
     List<Challenge> findAfterEndDateDesc(String today);
+
+    @Query("SELECT c FROM Challenge c WHERE c.startDate <= :today AND c.endDate >= :today ORDER BY c.participants.size DESC")
+    List<Challenge> findBetweenStartDateAndEndDateHot(String today);
+
+    List<Challenge> findByCategory(String category);
 }
