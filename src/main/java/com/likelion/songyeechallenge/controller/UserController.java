dc8d75b2780424cdad641f71f5c3cc3622ac14ba
newoceanwave <leeseohyun02@naver.com>
2023-11-21 17:07:13 +0900

    [feat] 숙명 이메일 필터링 (#22)

--- src/main/java/com/likelion/songyeechallenge/controller/UserController.java
+++ src/main/java/com/likelion/songyeechallenge/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.likelion.songyeechallenge.controller;
 
 import com.likelion.songyeechallenge.domain.user.User;
+import com.likelion.songyeechallenge.domain.validator.UserValidator;
 import com.likelion.songyeechallenge.user_dto.UserFormDto;
 import com.likelion.songyeechallenge.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 
     private final UserService userService;
+    private final UserValidator userValidator;
 
     @GetMapping("/signup")
     public String signup(Model model) {
@@ -24,9 +26,18 @@ public class UserController {
         return "signup";
     }
 
-
+    @InitBinder("userFormDto")
+    public void initBinder(WebDataBinder binder){
+        binder.addValidators(userValidator);
+    }
     @PostMapping("/signup")
-    public String signup(UserFormDto userFormDto) {
+    public String signup(@ModelAttribute("userFormDto") UserFormDto userFormDto, BindingResult result) {
+        userValidator.validate(userFormDto, result);
+
+        if (result.hasErrors()) {
+            return "signup";
+        }
+
         Long userId = userService.join(userFormDto);
         return "redirect:/login";
     }
