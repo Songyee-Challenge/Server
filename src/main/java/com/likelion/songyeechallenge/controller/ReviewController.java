e498ef77e53d6ffff6202591c1390ea5e1202f75
genieu99 <yujin0419@sookmyung.ac.kr>
2023-11-21 22:11:26 +0900

    [Chore] 리뷰 작성 테스트 성공 (#23)

--- src/main/java/com/likelion/songyeechallenge/controller/ReviewController.java
+++ src/main/java/com/likelion/songyeechallenge/controller/ReviewController.java
@@ -6,10 +6,7 @@ import com.likelion.songyeechallenge.web.dto.ReviewChallengeDto;
 import com.likelion.songyeechallenge.web.dto.ReviewSaveRequestDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +23,7 @@ public class ReviewController {
 //    }
 
     @PostMapping("/post")
-    public Long postReview(ReviewSaveRequestDto requestDto) {
+    public Long postReview(@RequestBody ReviewSaveRequestDto requestDto) {
         Review review = reviewService.postReview(requestDto);
         return review.getReview_id();
     }
