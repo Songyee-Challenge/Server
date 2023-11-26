c8958fbe9e621c131e8dfb731c631839d3ba1549
genieu99 <yujin0419@sookmyung.ac.kr>
2023-11-27 01:29:47 +0900

    [Chore] 메인페이지 상위 n개만 전송하도록 추가 (#85)

--- src/main/java/com/likelion/songyeechallenge/service/MainPageService.java
+++ src/main/java/com/likelion/songyeechallenge/service/MainPageService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -18,27 +17,27 @@ public class MainPageService {
 
     private LocalDate today = LocalDate.now();
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-    private String formattedToday = today.format(formatter);
+    private String formatedToday = today.format(formatter);
 
     private final ChallengeRepository challengeRepository;
 
     public List<ChallengeListResponseDto> findImminentPost() {
-        return findChallengesByStatus(() -> challengeRepository.findImminent(formattedToday));
+        List<Challenge> challenges = challengeRepository.findImminent(formatedToday);
+        return challenges.stream().limit(4)
+                .map(ChallengeListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public List<ChallengeListResponseDto> findHotInProcessPost() {
-        return findChallengesByStatus(() -> challengeRepository.findBeforeStartHot(formattedToday));
+        List<Challenge> challenges = challengeRepository.findBeforeStartHot(formatedToday);
+        return challenges.stream().limit(8)
+                .map(ChallengeListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public List<ChallengeListResponseDto> findByCategory(String category) {
         List<Challenge> challenges = challengeRepository.findByCategory(category);
         return challenges.stream()
-                .map(ChallengeListResponseDto::new)
-                .collect(Collectors.toList());
-    }
-
-    private List<ChallengeListResponseDto> findChallengesByStatus(Supplier<List<Challenge>> challengeList) {
-        return challengeList.get().stream()
                 .map(ChallengeListResponseDto::new)
                 .collect(Collectors.toList());
     }
