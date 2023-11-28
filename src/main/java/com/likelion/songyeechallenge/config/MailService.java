4bbb19c02c10835aee14e387a7a0eb4053f78872
genieu99 <yujin0419@sookmyung.ac.kr>
2023-11-28 18:57:11 +0900

    [Add] 이메일 인증 부분 디자인 추가

--- src/main/java/com/likelion/songyeechallenge/config/MailService.java
+++ src/main/java/com/likelion/songyeechallenge/config/MailService.java
@@ -1,12 +1,18 @@
 package com.likelion.songyeechallenge.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MailService {
@@ -25,14 +31,21 @@ public class MailService {
         MimeMessage message = javaMailSender.createMimeMessage();
 
         try {
+            Multipart multipart = new MimeMultipart("related");
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+
             message.setFrom(senderEmail);
             message.setRecipients(MimeMessage.RecipientType.TO, mail);
-            message.setSubject("이메일 인증");
+            message.setSubject("송이의숲 회원가입 이메일 인증번호");
+
+            double imageWidth = 951;
+            int imageHeight = 540;
             String body = "";
-            body += "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
+            body += String.format("<img src=\"https://ifh.cc/g/MTkbzQ.jpg\" alt='Image' width='%.2f' height='%d'>", imageWidth, imageHeight);
+            body += "<h3>" + "요청하신 인증 번호는" + "</h3>";
             body += "<h1>" + number + "</h1>";
-            body += "<h3>" + "감사합니다." + "</h3>";
-            message.setText(body,"UTF-8", "html");
+            body += "<h3>" + "입니다. 감사합니다." + "</h3>";
+            messageHelper.setText(body,true);
         } catch (MessagingException e) {
             e.printStackTrace();
         }
