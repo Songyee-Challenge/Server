ab57d35f77665e47fca724656e64f292c3d5ff42
genieu99 <yujin0419@sookmyung.ac.kr>
2023-12-01 21:11:59 +0900

    [Fix] 참여한 미션 불러오기 오류 하나씩 해결해보기7 (#93)

--- src/main/java/com/likelion/songyeechallenge/domain/mission/MissionRepository.java
+++ src/main/java/com/likelion/songyeechallenge/domain/mission/MissionRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
     @Query("SELECT m FROM Mission m WHERE m.challenge.challenge_id = :challengeId")
     List<Mission> findByChallengeId(Long challengeId);
+
+    @Query("SELECT m FROM Mission m WHERE m.mission_id = :missionId")
+    Mission findByMissionId(Long missionId);
 }
--- src/main/java/com/likelion/songyeechallenge/web/dto/MyMissionCompleteDto.java
+++ src/main/java/com/likelion/songyeechallenge/web/dto/MyMissionCompleteDto.java
@@ -1,6 +1,7 @@
 package com.likelion.songyeechallenge.web.dto;
 
 import com.likelion.songyeechallenge.domain.mission.Mission;
+import com.likelion.songyeechallenge.domain.mission.MissionRepository;
 import com.likelion.songyeechallenge.domain.userMission.UserMission;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +21,7 @@ public class MyMissionCompleteDto {
     public MyMissionCompleteDto(UserMission userMission) {
         Mission missionEntity = userMission.getMission();
         this.mission_id = missionEntity.getMission_id();
+        log.info("미션 아이디: {}", mission_id);
         this.missionDate = missionEntity.getMissionDate();
         this.mission = missionEntity.getMission();
         this.isComplete = userMission.isComplete();
